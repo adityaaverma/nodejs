c3f166565ae1652b6148a909ba7a7c876922110b
Aditya Aryan Verma <adityaverma655@gmail.com>
2022-07-18 15:08:42 +0530

    added a new file

--- intro/file.js
+++ intro/file.js
@@ -1,1 +1,5 @@
-console.log("hello world")
+const lib=require("./lib.js")
+
+console.log(lib)
+
+lib.awesomeFunction()
---
+++ intro/lib.js
@@ -0,0 +1,6 @@
+function awesomeFunction(){
+    console.log("OMG! this is awesome!")
+}
+module.exports={
+    awesomeFunction
+}
